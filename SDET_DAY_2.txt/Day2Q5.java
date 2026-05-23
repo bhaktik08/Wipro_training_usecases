b71bded1761f8885569a3b5e38ada5de70a440ec
bhaktik08 <141610366+bhaktik08@users.noreply.github.com>
2026-05-23 16:45:19 +0530

    Create Day2Q5.java

---
+++ SDET_DAY_2.txt/Day2Q5.java
@@ -0,0 +1,25 @@
+package April_Batch;
+
+public class Day2Q5 {
+	public static void main(String[]args) {
+		int menu = 2;
+		switch(menu)
+		{
+		case 1:
+            System.out.println("You selected Pizza");
+            break;
+        case 2:
+            System.out.println("You selected Burger");
+            break;
+        case 3:
+            System.out.println("You selected Pasta");
+            break;
+        case 4:
+            System.out.println("You selected Sandwich");
+            break;
+        default:
+            System.out.println("Invalid choice");
+		}
+	}
+
+}
