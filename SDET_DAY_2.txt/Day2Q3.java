d350b331fe2075b352b46296dd140313be35f75a
bhaktik08 <141610366+bhaktik08@users.noreply.github.com>
2026-05-23 16:43:33 +0530

    Create Day2Q3.java

---
+++ SDET_DAY_2.txt/Day2Q3.java
@@ -0,0 +1,15 @@
+package April_Batch;
+
+public class Day2Q3 {
+	static int balance = 4377;
+	public static void main(String [] args) {
+		
+		if(balance > 0) {
+			System.out.println("you can withdraw a money");
+		}
+		else {
+			System.out.println("your balance is not sufficient.");
+		}
+	}
+
+}
