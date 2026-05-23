44c5023518fa1d855b900a9b965bda57a6781eba
bhaktik08 <141610366+bhaktik08@users.noreply.github.com>
2026-05-23 16:43:01 +0530

    Create Day2Q2.java

---
+++ SDET_DAY_2.txt/Day2Q2.java
@@ -0,0 +1,18 @@
+package April_Batch;
+
+public class Day2Q2 {
+	
+	static String username = "Bhakti";
+	static int password = 12345;
+	
+	public static void main(String [] args) {
+		if(username == "Bhakti"  && password == 12345) {
+			System.out.println("Success");
+		}
+		
+		else {
+			System.out.println("Failure");
+		}
+
+	}
+}
