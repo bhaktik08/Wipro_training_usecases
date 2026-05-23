4cfa5f143d66d58c4e78fd3d83ad8f924ea497fe
bhaktik08 <141610366+bhaktik08@users.noreply.github.com>
2026-05-23 16:09:30 +0530

    Create Student_info.java

---
+++ SDET_DAY_1.txt/Student_info.java
@@ -0,0 +1,20 @@
+package Stude_details;
+
+public class StudentInfo {
+	public static void main(String[] args) {
+	
+	String name ="Bhakti";
+	int age = 22;
+	float subm1 = 98, subm2 = 87, subm3 = 91, average =(subm1+subm2+subm3)/3;
+
+	
+
+	System.out.println("Student details");
+	
+	System.out.println("Name: " + name);
+	System.out.println("Age: " + age);
+	System.out.println("Average Marks: "+ average);
+	
+	}
+
+}
