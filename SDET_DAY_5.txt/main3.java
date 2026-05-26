db43d20c0b4ad8a127c802704b76303b334edfb4
bhaktik08 <141610366+bhaktik08@users.noreply.github.com>
2026-05-26 16:50:18 +0530

    Create main3.java

---
+++ SDET_DAY_5.txt/main3.java
@@ -0,0 +1,21 @@
+package day5_assignment;
+
+final class SecurityModule {
+
+    public void encryptData() {
+        System.out.println("Data is encrypted securely");
+    }
+}
+
+
+//   class AdvancedSecurity extends SecurityModule {
+//     }
+
+
+public class main3 {
+	  public static void main(String[] args) {
+
+	        SecurityModule obj = new SecurityModule();
+	        obj.encryptData();
+	    }
+}
